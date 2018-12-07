8cd83095116ed3ac12d0301d3b5966c7a728b592
NANDANNANDAN <45446740+NANDANNANDAN@users.noreply.github.com>
2018-12-07 11:47:09 +0530

    Update u6uyu.java

--- u6uyu.java
+++ u6uyu.java
@@ -5,7 +5,6 @@ class Largest
   public static void main(String args[])
   {
     int x, y, z;
-    System.out.println("Enter three integers");
     Scanner in = new Scanner(System.in);
  
     x = in.nextInt();
