d502da0b2fd411287a13afc88fb52f425d92ac3f
NANDANNANDAN <45446740+NANDANNANDAN@users.noreply.github.com>
2018-12-07 11:01:30 +0530

    Create pos.java

---
+++ pos.java
@@ -0,0 +1,19 @@
+class Number 
+{
+public static void main(String args[])
+{
+int number=3;
+if(number>0)
+{
+System.out.println("Positive");
+}
+else if(number<0)
+{
+System.out.println("Negative");
+}
+else
+{
+System.out.println("Zero");
+}
+}
+}
